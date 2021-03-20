66d4f0e89f14c8c8bf7a19a93c3be04cd5babbe6
widal001 <williamdaly422@gmail.com>
2021-03-20 18:43:10 -0400

    Updates the index.html with basic description of the project

--- mapper/webapp/webapp.py
+++ mapper/webapp/webapp.py
@@ -1,6 +1,12 @@
 from flask import Blueprint, render_template
+from datetime import datetime
 
 webapp_bp = Blueprint("webapp", __name__, template_folder="templates")
+
+
+@webapp_bp.context_processor
+def inject_now():
+    return {"now": datetime.utcnow()}
 
 
 @webapp_bp.route("/")
